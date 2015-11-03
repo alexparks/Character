1cc70454a32f2533b5c736d13d24f7acf780af72
alexparks <alexander.parks@brentwood.ca>
2015-11-03 12:27:22 -0800

    new eyes

--- src/character/Stan.java
+++ src/character/Stan.java
@@ -15,20 +15,21 @@ import java.awt.Point;
  * @author Alex Parks
  */
 public class Stan {
-
+    
     private int x;
     private int y;
     Grid grid;
     private static int ROWS = 20;
     private static int COLUMNS = 20;
-
+    
     public Stan(int x, int y, int width, int height) {
         grid = new Grid(ROWS, COLUMNS, width / COLUMNS, height / ROWS, new Point(x, y), Color.BLACK);
         this.x = x;
         this.y = y;
     }
-
+    
     public void draw(Graphics graphics) {
+
         //head
         graphics.setColor(new Color(255, 225, 195));
         graphics.fillOval(x, y, 400, 400);
@@ -36,24 +37,28 @@ public class Stan {
 //<editor-fold defaultstate="collapsed" desc="Eyes Left">
         //eyes left
         graphics.setColor(Color.WHITE);
-        graphics.fillOval(x + 115, y + 160, 75, 100);
+        graphics.fillOval(x + 115, y + 175, 75, 100);
         graphics.setColor(Color.BLACK);
-        graphics.fillOval(x + 160, y + 200, 10, 10);
+        graphics.fillOval(x + 160, y + 215, 10, 10);
 //</editor-fold>
-        
+
 //<editor-fold defaultstate="collapsed" desc="Eyes Right">
         //eyes right
         graphics.setColor(Color.WHITE);
-        graphics.fillOval(x + 210, y + 160, 75, 100);
+        graphics.fillOval(x + 210, y + 175, 75, 100);
         graphics.setColor(Color.BLACK);
-        graphics.fillOval(x + 230, y + 200, 10, 10);
+        graphics.fillOval(x + 230, y + 215, 10, 10);
 //</editor-fold>
+
         //hat
-//        graphics.fillArc(x, y, x, x, x, x);
+        graphics.drawRect(x + 3, y + 135, 400, 25);
+
+        //mouth
+//       graphics.drawArc(x, y, x, x, x, x);
         //grid
         if (grid != null) {
             grid.paintComponent(graphics);
         }
     }
-
+    
 }
